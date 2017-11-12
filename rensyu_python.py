2114810a43856552d0a6b13800e849ba56a73bd4
Kumiko Motomiya <mana.love.hero@gmail.com>
2017-11-12 20:03:38 +0900

    マップの描画関数を追加

--- rensyu_python.py
+++ rensyu_python.py
@@ -1,5 +1,12 @@
 import tkinter
 
+# マップの描画関数
+def draw_map():
+    for y in range(0, MAX_HEIGHT):
+        for x in range(0, MAX_WIDTH):
+            p = map_data[y][x]
+            canvas.create_image(x*62+31, y*62+31, image=images[p])
+
 # ウィンドウ作成
 root = tkinter.Tk()
 root.title("ダンジョン＆パイソン")
@@ -25,7 +32,9 @@ map_data = [[1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
             [1, 0, 0, 1, 2, 0, 0, 1, 3, 1],
             [1, 1, 0, 1, 1, 1, 0, 1, 0, 1],
             [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+            [1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
+draw_map()
 root.mainloop()
